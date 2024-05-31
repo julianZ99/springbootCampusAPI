bc05d13e61138cb111b87b2695c2ec130b013668
Julian Zapata <zapatajulian79@gmail.com>
2024-05-30 22:21:58 -0300

    added roles

--- src/main/java/com/example/demo/models/entity/Privilege.java
+++ src/main/java/com/example/demo/models/entity/Privilege.java
@@ -2,18 +2,21 @@ package com.example.demo.models.entity;
 
 import java.util.Collection;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Privilege {
- 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/java/com/example/demo/models/entity/Role.java
+++ src/main/java/com/example/demo/models/entity/Role.java
@@ -2,20 +2,23 @@ package com.example.demo.models.entity;
 
 import java.util.Collection;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Role {
- 
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
